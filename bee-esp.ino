138f6ac67c7d3241a63477e0518e3a24102c97f7
Quang Dang <88885581+Gnauqoa@users.noreply.github.com>
2024-12-08 22:32:49 +0700

    Refactor to camel case.

--- bee-esp.ino
+++ bee-esp.ino
@@ -1,5 +1,5 @@
-#define BLYNK_TEMPLATE_ID "TMPL6Br9XbE2_"
-#define BLYNK_TEMPLATE_NAME "sau"
+#define blynkTemplateId "TMPL6Br9XbE2_"
+#define blynkTemplateName "sau"
 
 #include <ESP8266WiFi.h>
 #include <WiFiManager.h>  // https://github.com/tzapu/WiFiManager
@@ -8,17 +8,17 @@
 
 const char blynkAuth[] = "khn2mK6HNos9yQKCkyDT1pBPesFCYJd1";  // Blynk Auth Token
 
-WiFiManager wifi_manager;
-ESP8266WebServer server(80);  // Web server on port 80
+WiFiManager wifiManager;
+ESP8266WebServer webServer(80);  // Web server on port 80
 
-String incoming_data = "";
-String serial_log = "";  // To store serial log
-bool debug_server = false;
-bool control_mode = 0;
+String incomingDataStr = "";
+String serialLog = "";  // To store serial log
+bool debugServer = false;
+bool controlMode = 0;
 char incomingData[64];  // Array to hold incoming data
-float temp_range = 0;
-float humi_range = 0;
-float lux_range = 0;
+float tempRange = 0;
+float humiRange = 0;
+float luxRange = 0;
 uint8_t mosfetSpeed = 100;
 
 int relayState1 = 0;
@@ -32,28 +32,27 @@ void handleRoot();  // Web server root handler
 void sendToArduino();
 byte calculateChecksum(byte* data, size_t length);
 
-
 void setup() {
   Serial.begin(9600);
   Serial.println("ESP8266 Blynk Connection");
 
   // Set up WiFiManager for automatic connection
-  wifi_manager.autoConnect("ESP8266_Blynk");
+  wifiManager.autoConnect("ESP8266_Blynk");
   Serial.println("WiFi connected.");
 
   // Initialize Blynk
   Blynk.begin(blynkAuth, WiFi.SSID().c_str(), WiFi.psk().c_str());
 
   // Start the Web Server
-  if (debug_server) {
-    server.on("/", handleRoot);  // Define root endpoint
-    server.begin();
+  if (debugServer) {
+    webServer.on("/", handleRoot);  // Define root endpoint
+    webServer.begin();
   }
 }
 
 void loop() {
-  Blynk.run();            // Keep Blynk active
-  server.handleClient();  // Handle web server requests
+  Blynk.run();               // Keep Blynk active
+  webServer.handleClient();  // Handle web server requests
   readSerial();
 }
 
@@ -72,9 +71,6 @@ void readSerial() {
       int result = sscanf(incomingData, "s,%f,%f,%f",
                           &temperature, &humidity, &lux);
       if (result == 3) {  // Successfully parsed all expected values
-        // Output received values
-
-
         // Send the data to Blynk (Virtual Pins)
         Blynk.virtualWrite(V0, temperature);  // Send temperature to V0
         Blynk.virtualWrite(V1, humidity);     // Send humidity to V1
@@ -83,13 +79,10 @@ void readSerial() {
     } else if (strstr(incomingData, "d,") != NULL) {
       int relay1 = 0, relay2 = 0, relay3 = 0, relay4 = 0;
 
-      // Parse the string data: "s,25.5,60.2,123.4,relay1State,relay2State,...,mosfetSpeed"
       int result = sscanf(incomingData, "d,%d,%d,%d,%d,%d",
                           &relay1, &relay2, &relay3, &relay4, &mosfetSpeed);
-      
-      if (result == 5) {  // Successfully parsed all expected values
-        // Output received values
 
+      if (result == 5) {                      // Successfully parsed all expected values
         Blynk.virtualWrite(V6, relay1);       // Send Relay 1 state to V6
         Blynk.virtualWrite(V7, relay2);       // Send Relay 2 state to V7
         Blynk.virtualWrite(V8, relay3);       // Send Relay 3 state to V8
@@ -99,6 +92,7 @@ void readSerial() {
     }
   }
 }
+
 byte calculateChecksum(byte* data, size_t length) {
   byte checksum = 0;
   for (size_t i = 0; i < length; i++) {
@@ -111,62 +105,60 @@ byte calculateChecksum(byte* data, size_t length) {
 void handleRoot() {
   String html = "<html><head><title>ESP8266 Serial Log</title></head><body>";
   html += "<h1>ESP8266 Serial Log</h1>";
-  html += "<div style='white-space: pre-wrap; font-family: monospace;'>" + serial_log + "</div>";
+  html += "<div style='white-space: pre-wrap; font-family: monospace;'>" + serialLog + "</div>";
   html += "</body></html>";
-  serial_log = "";
-  server.send(200, "text/html", html);
+  serialLog = "";
+  webServer.send(200, "text/html", html);
 }
 
 void sendToArduino() {
-  String message = "g," + String(temp_range) + "," + String(humi_range) + "," + String(lux_range) + String(control_mode) + "," + String(mosfetSpeed);
-  // Add relay states to the message
+  String message = "g," + String(tempRange) + "," + String(humiRange) + "," + String(luxRange) + String(controlMode) + "," + String(mosfetSpeed);
   message += "," + String(relayState1) + "," + String(relayState2) + "," + String(relayState3) + "," + String(relayState4);
 
   Serial.println(message);  // Send the formatted message to Arduino over serial
 }
 
-
-BLYNK_WRITE(V5) {               // Virtual Pin V5 is assigned to input number widget
-  mosfetSpeed = param.asInt();  // Set the temperature range from the app
+BLYNK_WRITE(V5) {
+  mosfetSpeed = param.asInt();
   sendToArduino();
 }
 
-BLYNK_WRITE(V4) {                  // Virtual Pin V4 is assigned to button widget
-  control_mode = !!param.asInt();  // 0 = Off, 1 = On
+BLYNK_WRITE(V4) {
+  controlMode = !!param.asInt();
   sendToArduino();
 }
 
-BLYNK_WRITE(V3) {                // Virtual Pin V3 is assigned to input number widget
-  temp_range = param.asFloat();  // Set the temperature range from the app
+BLYNK_WRITE(V3) {
+  tempRange = param.asFloat();
   sendToArduino();
 }
 
-BLYNK_WRITE(V10) {                // Virtual Pin V3 is assigned to input number widget
-  humi_range = param.asFloat();  // Set the temperature range from the app
+BLYNK_WRITE(V10) {
+  humiRange = param.asFloat();
   sendToArduino();
 }
 
-BLYNK_WRITE(V11) {                // Virtual Pin V3 is assigned to input number widget
-  light_range = param.asFloat();  // Set the temperature range from the app
+BLYNK_WRITE(V11) {
+  luxRange = param.asFloat();
   sendToArduino();
 }
 
-BLYNK_WRITE(V6) {               // Virtual Pin V6 controls Relay 1
-  relayState1 = param.asInt();  // Update the state of relay 1
+BLYNK_WRITE(V6) {
+  relayState1 = param.asInt();
   sendToArduino();
 }
 
-BLYNK_WRITE(V7) {               // Virtual Pin V7 controls Relay 2
-  relayState2 = param.asInt();  // Update the state of relay 2
+BLYNK_WRITE(V7) {
+  relayState2 = param.asInt();
   sendToArduino();
 }
 
-BLYNK_WRITE(V8) {               // Virtual Pin V8 controls Relay 3
-  relayState3 = param.asInt();  // Update the state of relay 3
+BLYNK_WRITE(V8) {
+  relayState3 = param.asInt();
   sendToArduino();
 }
 
-BLYNK_WRITE(V9) {               // Virtual Pin V9 controls Relay 4
-  relayState4 = param.asInt();  // Update the state of relay 4
+BLYNK_WRITE(V9) {
+  relayState4 = param.asInt();
   sendToArduino();
 }
